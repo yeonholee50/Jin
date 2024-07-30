ac3fb1bdb1f2e443f6a0d3d879edad6cc9887a5d
Yeon Lee <yeonholee50@gmail.com>
2024-07-30 14:21:42 -0400

    added log messages with test client

---
+++ slackbot.py
@@ -0,0 +1,29 @@
+import slack
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+import logging
+import socket
+import time
+from flask import Flask
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    
+    client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
+    
+    if client:
+        logging.info("Jin has successfully connected")
+    else:
+        logging.error("Jin has failed to connect")
+    while True:
+        time.sleep(10)
+        client.chat_postMessage(channel="#testbot", text="Hello World")
+        logging.info("Hello World Posted")
+
+if __name__ == "__main__":
+    
+    main()
