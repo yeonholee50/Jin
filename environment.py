228e0bdc8dd66b9718b9cf05c59b6abb418bbe5e
Yeon Lee <82836242+yeonholee50@users.noreply.github.com>
2024-07-31 16:48:11 -0400

    Update environment.py

--- environment.py
+++ environment.py
@@ -1,3 +1,6 @@
 SLACK_TOKEN = "xoxb-7524054339696-7486910676711-oyf6QB64S0lOMOWDc5p5GmxV"
 SIGNING_SECRET = "deb04cb163d7b119d589b7ca21c79a7f"
 MONGO_DB = "mongodb+srv://yeonholee50:yeonholee50@cluster0.n6anaol.mongodb.net/"
+
+# Congrats, you found my keys. Normally, you would want to put them as secret tokens in your server or .env file, but since git doesn't let me push a .env and we're still in development stage, these keys are public.
+# But this would be a good way for someone to access by database and gain access to my slack channel which isn't fun, so please use encrypted secret token on your server
