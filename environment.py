bc877ed89b3e32540fe38a8081516e8ec4a1fd24
Yeon Lee <82836242+yeonholee50@users.noreply.github.com>
2024-07-31 16:46:14 -0400

    Update environment.py to be secure on main

--- environment.py
+++ environment.py
@@ -1,3 +1,3 @@
-SLACK_TOKEN = "xoxb-7524054339696-7486910676711-oyf6QB64S0lOMOWDc5p5GmxV"
-SIGNING_SECRET = "deb04cb163d7b119d589b7ca21c79a7f"
-MONGO_DB = "mongodb+srv://yeonholee50:yeonholee50@cluster0.n6anaol.mongodb.net/"
+SLACK_TOKEN = "slack_token"
+SIGNING_SECRET = "slack_signing_secret"
+MONGO_DB = "mongo_key"
